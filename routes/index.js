0eff4990e5be199f26d4e58931e887b060400738
José Francisco Aguirre Villarroel <jofravimusic@byui.edu>
2022-06-21 21:24:48 -0300

    added get user route

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ routes.get('/', (req, res) => {
   res.send(`Welcome to our API. Use this routes: /user, /invoice, /patient and /appointment. More information in /api-docs
   `);
 });
-//routes.use('/user', require('./user'));
+routes.use('/user', require('./user'));
 //routes.use('/patient', require('./patient'));
 //routes.use('/invoice', require('./invoice'));
 //routes.use('/appointment', require('./appointment'));
