65d6be17a99591cddc41b9941aabf056e0a3addc
José Francisco Aguirre Villarroel <jofravimusic@byui.edu>
2022-06-24 21:40:10 -0300

    patient route updated and swagger

--- routes/patient.js
+++ routes/patient.js
@@ -31,89 +31,93 @@ routes.get('/', (req, res) => {
     console.log('All Patients shown');
   });
 });
-/*
-// Create a user
-routes.post('/', userValidation, (req, res) => {
-  const result = results(req);
+
+// Create a patient
+routes.post('/', (req, res) => {
+  /*const result = results(req);
   if (!result.isEmpty()) {
     const errors = result.array();
     return res.status(400).json(errors);
-  }
+  }*/
 
-  const user = dbconnection.getUsers().insertOne({
+  const patient = dbconnection.getPatient().insertOne({
+    googleID: req.body.googleID,
+    displayName: req.body.displayName,
     firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
+    image: req.body.image,
+    birth: req.body.birth,
+    createdAt: req.body.createdAt,
     role: req.body.role,
-    password: req.body.password,
   });
 
-  user.then((document) => {
+  patient.then((document) => {
     if (!document.insertedId) return res.status(404).send(`No user was added`);
-    res.status(201).redirect(`/users/${document.insertedId}`);
+    res.status(201).redirect(`/patient/${document.insertedId}`);
     console.log(`User was created with id: ${document.insertedId}`);
   });
 });
 
-// Get a user by Id
+// Get a patient by Id
 routes.get('/:id', (req, res) => {
   const passedId = req.params.id;
-  if (!ObjectId.isValid(passedId)) {
+  /*if (!ObjectId.isValid(passedId)) {
     const error = createError(400, 'Invalid Id provided');
     return res.status(error.status).send(error);
-  }
-  const userId = new ObjectId(passedId);
+  }*/
+  const patientId = new ObjectId(passedId);
 
-  const user = dbconnection.getUsers().findOne({ _id: userId });
+  const patient = dbconnection.getPatient().findOne({ _id: patientId });
 
-  user.then((document) => {
+  patient.then((document) => {
     if (!document)
-      return res.status(404).send(`No user with id: ${req.params.id}`);
+      return res.status(404).send(`No patient with id: ${req.params.id}`);
     res.status(200).json(document);
-    console.log(`User was retrieved with id: ${req.params.id}`);
+    console.log(`Patient was retrieved with id: ${req.params.id}`);
   });
 });
 
-// Update a user by Id
-routes.put('/:id', userValidation, (req, res) => {
+// Update a patient by Id
+routes.put('/:id', (req, res) => {
   const passedId = req.params.id;
-  if (!ObjectId.isValid(passedId)) {
+  /*if (!ObjectId.isValid(passedId)) {
     const error = createError(400, 'Invalid Id provided');
     return res.status(error.status).send(error);
-  }
-  const userId = new ObjectId(passedId);
-
+  }*/
+  const patientId = new ObjectId(passedId);
+  /*
   const result = results(req);
   if (!result.isEmpty()) {
     const errors = result.array();
     return res.status(400).json(errors);
   }
-
-  const user = dbconnection.getUsers().updateOne(
+*/
+  const patient = dbconnection.getPatient().updateOne(
     {
-      _id: userId,
+      _id: patientId,
     },
     {
       $set: {
+        googleID: req.body.googleID,
+        displayName: req.body.displayName,
         firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
+        image: req.body.image,
+        birth: req.body.birth,
+        createdAt: req.body.createdAt,
         role: req.body.role,
-        password: req.body.password,
       },
     }
   );
 
-  user.then((document) => {
+  patient.then((document) => {
     if (document.matchedCount >= 1) {
       if (document.modifiedCount < 1)
         return res
           .status(404)
-          .send('User could not be updated, nothing was changed');
+          .send('Patient could not be updated, nothing was changed');
       res.status(201).json(document);
-      console.log(`User was updated with id: ${req.params.id}`);
+      console.log(`Patient was updated with id: ${req.params.id}`);
     } else {
-      res.status(404).send(`User was not found with id: ${req.params.id}`);
+      res.status(404).send(`Patient was not found with id: ${req.params.id}`);
     }
   });
 });
@@ -121,21 +125,23 @@ routes.put('/:id', userValidation, (req, res) => {
 // Delete a user by Id
 routes.delete('/:id', (req, res) => {
   const passedId = req.params.id;
-  if (!ObjectId.isValid(passedId)) {
+  /*if (!ObjectId.isValid(passedId)) {
     const error = createError(400, 'Invalid Id provided');
     return res.status(error.status).send(error);
-  }
-  const userId = new ObjectId(passedId);
-  const user = dbconnection.getUsers().deleteOne({ _id: userId });
+  }*/
+  const patientId = new ObjectId(passedId);
+  const patient = dbconnection.getPatient().deleteOne({ _id: patientId });
 
-  user.then((document) => {
+  patient.then((document) => {
     if (document.deletedCount < 1)
       return res
         .status(404)
-        .send(`No user with id: ${req.params.id} or deleted was not processed`);
+        .send(
+          `No patient with id: ${req.params.id} or deleted was not processed`
+        );
     res.status(200).json(document);
-    console.log(`User was deleted with id: ${req.params.id}`);
+    console.log(`Patient was deleted with id: ${req.params.id}`);
   });
 });
-*/
+
 module.exports = routes;
