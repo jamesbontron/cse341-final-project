250b2a309f1e5b4310f67d29a09a06fa6bf2fa18
José Francisco Aguirre Villarroel <jofravimusic@byui.edu>
2022-06-24 23:34:46 -0300

    invoice route and swagger updated

--- routes/invoice.js
+++ routes/invoice.js
@@ -22,7 +22,7 @@ routes.use((req, res, next) => {
   next();
 });
 
-// Get all users
+// Get all Invoices
 routes.get('/', (req, res) => {
   const invoice = dbconnection.getInvoice().find();
 
@@ -31,89 +31,96 @@ routes.get('/', (req, res) => {
     console.log('All users shown');
   });
 });
-/*
-// Create a user
-routes.post('/', userValidation, (req, res) => {
-  const result = results(req);
+
+// Create an Invoice
+routes.post('/', (req, res) => {
+  /*const result = results(req);
   if (!result.isEmpty()) {
     const errors = result.array();
     return res.status(400).json(errors);
-  }
+  }*/
 
-  const user = dbconnection.getUsers().insertOne({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    role: req.body.role,
-    password: req.body.password,
+  const invoice = dbconnection.getInvoice().insertOne({
+    date: req.body.date,
+    hour: req.body.hour,
+    doctorId: req.body.doctorId,
+    patientId: req.body.patientId,
+    appointmentId: req.body.appointmentId,
+    doctorComments: req.body.doctorComments,
+    status: req.body.status,
+    price: req.body.price,
   });
 
-  user.then((document) => {
-    if (!document.insertedId) return res.status(404).send(`No user was added`);
-    res.status(201).redirect(`/users/${document.insertedId}`);
-    console.log(`User was created with id: ${document.insertedId}`);
+  invoice.then((document) => {
+    if (!document.insertedId)
+      return res.status(404).send(`No invoice was added`);
+    res.status(201).redirect(`/invoice/${document.insertedId}`);
+    console.log(`Invoice was created with id: ${document.insertedId}`);
   });
 });
 
-// Get a user by Id
+// Get an invoice by Id
 routes.get('/:id', (req, res) => {
   const passedId = req.params.id;
-  if (!ObjectId.isValid(passedId)) {
+  /*if (!ObjectId.isValid(passedId)) {
     const error = createError(400, 'Invalid Id provided');
     return res.status(error.status).send(error);
-  }
-  const userId = new ObjectId(passedId);
+  }*/
+  const invoiceId = new ObjectId(passedId);
 
-  const user = dbconnection.getUsers().findOne({ _id: userId });
+  const invoice = dbconnection.getInvoice().findOne({ _id: invoiceId });
 
-  user.then((document) => {
+  invoice.then((document) => {
     if (!document)
-      return res.status(404).send(`No user with id: ${req.params.id}`);
+      return res.status(404).send(`No invoice with id: ${req.params.id}`);
     res.status(200).json(document);
-    console.log(`User was retrieved with id: ${req.params.id}`);
+    console.log(`Invoice was retrieved with id: ${req.params.id}`);
   });
 });
 
 // Update a user by Id
-routes.put('/:id', userValidation, (req, res) => {
+routes.put('/:id', (req, res) => {
   const passedId = req.params.id;
-  if (!ObjectId.isValid(passedId)) {
+  /*if (!ObjectId.isValid(passedId)) {
     const error = createError(400, 'Invalid Id provided');
     return res.status(error.status).send(error);
-  }
-  const userId = new ObjectId(passedId);
-
+  }*/
+  const invoiceId = new ObjectId(passedId);
+  /*
   const result = results(req);
   if (!result.isEmpty()) {
     const errors = result.array();
     return res.status(400).json(errors);
-  }
+  }*/
 
-  const user = dbconnection.getUsers().updateOne(
+  const invoice = dbconnection.getInvoice().updateOne(
     {
-      _id: userId,
+      _id: invoiceId,
     },
     {
       $set: {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        role: req.body.role,
-        password: req.body.password,
+        date: req.body.date,
+        hour: req.body.hour,
+        doctorId: req.body.doctorId,
+        patientId: req.body.patientId,
+        appointmentId: req.body.appointmentId,
+        doctorComments: req.body.doctorComments,
+        status: req.body.status,
+        price: req.body.price,
       },
     }
   );
 
-  user.then((document) => {
+  invoice.then((document) => {
     if (document.matchedCount >= 1) {
       if (document.modifiedCount < 1)
         return res
           .status(404)
-          .send('User could not be updated, nothing was changed');
+          .send('Invoice could not be updated, nothing was changed');
       res.status(201).json(document);
-      console.log(`User was updated with id: ${req.params.id}`);
+      console.log(`Invoice was updated with id: ${req.params.id}`);
     } else {
-      res.status(404).send(`User was not found with id: ${req.params.id}`);
+      res.status(404).send(`Invoice was not found with id: ${req.params.id}`);
     }
   });
 });
@@ -121,21 +128,23 @@ routes.put('/:id', userValidation, (req, res) => {
 // Delete a user by Id
 routes.delete('/:id', (req, res) => {
   const passedId = req.params.id;
-  if (!ObjectId.isValid(passedId)) {
+  /*if (!ObjectId.isValid(passedId)) {
     const error = createError(400, 'Invalid Id provided');
     return res.status(error.status).send(error);
-  }
-  const userId = new ObjectId(passedId);
-  const user = dbconnection.getUsers().deleteOne({ _id: userId });
+  }*/
+  const invoiceId = new ObjectId(passedId);
+  const invoice = dbconnection.getInvoice().deleteOne({ _id: invoiceId });
 
-  user.then((document) => {
+  invoice.then((document) => {
     if (document.deletedCount < 1)
       return res
         .status(404)
-        .send(`No user with id: ${req.params.id} or deleted was not processed`);
+        .send(
+          `No invoice with id: ${req.params.id} or deleted was not processed`
+        );
     res.status(200).json(document);
-    console.log(`User was deleted with id: ${req.params.id}`);
+    console.log(`invoice was deleted with id: ${req.params.id}`);
   });
 });
-*/
+
 module.exports = routes;
