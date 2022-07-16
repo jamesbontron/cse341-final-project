fac318c1579b73db4d9964f73f941dfed2535622
José Francisco Aguirre Villarroel <jofravimusic@byui.edu>
2022-07-16 02:24:52 -0300

    POST test on Invoice

--- test/api/medical.route.spec.js
+++ test/api/medical.route.spec.js
@@ -26,4 +26,18 @@ describe('Test Medical Appointment Scheduler API', () => {
   it('GET /invoice 200 OK', async () => {
     await api.get('/invoice').expect(200);
   });
+
+  it('POST /invoice 200 OK', async () => {
+    await api.get('/invoice').expect(200).send({
+      date: '2022-07-16',
+      hour: '20:26',
+      doctorId: '62cc3fd838c349c8b1e90ffb',
+      patientId: '62ce153b3dd916a60de8277c',
+      appointmentId: '62d0a5bff2da0e802cd53a0b',
+      doctorComments:
+        'Reposo por 72 horas, Amoxicilina por 1 semana, 1 comprimido cada 8 horas',
+      status: 'Finished',
+      price: '15',
+    });
+  });
 });
